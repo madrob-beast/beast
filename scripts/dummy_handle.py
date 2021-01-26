2c47106029f081dc6eb026ee64cc08bdd2813442
Enri2077 <enry2077@gmail.com>
2021-01-26 09:23:45 +0100

    implemented dummy with latest msg and srv types

--- scripts/dummy_handle.py
+++ scripts/dummy_handle.py
@@ -5,22 +5,21 @@ import rospy
 from beast_msgs.msg import *
 from beast_srvs.srv import *
 
+
 def main(): 
     rospy.init_node('handle')
 
-    force_pub = rospy.Publisher('~force', Handle, queue_size=10)
+    force_pub = rospy.Publisher('/handle', Handle, queue_size=1)
 
     rate = rospy.Rate(50)
 
     while not rospy.is_shutdown():
         msg = Handle()
         msg.header.stamp = rospy.Time.now()
-
         msg.force = random.random() * 20
-        
         force_pub.publish(msg)
-
         rate.sleep()
+
 
 if __name__ == "__main__":
     main()
